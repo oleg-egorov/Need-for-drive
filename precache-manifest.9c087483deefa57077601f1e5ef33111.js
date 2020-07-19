41bfdac3eadef0068c82c187a3124b9ca311d77d
Oleg944 <legek99@yandex.ru>
2020-07-19 21:27:24 +0300

    Updates

---
+++ precache-manifest.9c087483deefa57077601f1e5ef33111.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6dffc32420cb57ed78e44df9eb03a2c",
+    "url": "/Need-for-drive/index.html"
+  },
+  {
+    "revision": "7817d0dc0b06ca27f31b",
+    "url": "/Need-for-drive/static/css/main.da92d992.chunk.css"
+  },
+  {
+    "revision": "1a156168eff1ae3b4072",
+    "url": "/Need-for-drive/static/js/2.c9f029a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Need-for-drive/static/js/2.c9f029a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7817d0dc0b06ca27f31b",
+    "url": "/Need-for-drive/static/js/main.98b53745.chunk.js"
+  },
+  {
+    "revision": "a083fbd5d459759fd6dc",
+    "url": "/Need-for-drive/static/js/runtime-main.6cef0d6b.js"
+  },
+  {
+    "revision": "e249e091dcc4fd54f867d0df6f9a6f5f",
+    "url": "/Need-for-drive/static/media/1.e249e091.jpg"
+  },
+  {
+    "revision": "0478545f714cca406ea545187d3c2224",
+    "url": "/Need-for-drive/static/media/2.0478545f.jpg"
+  },
+  {
+    "revision": "57281155d59e3f24aa66d6f41ebfa3f2",
+    "url": "/Need-for-drive/static/media/3.57281155.jpg"
+  },
+  {
+    "revision": "82423b3f0505a2b72719dec80d9fbb5e",
+    "url": "/Need-for-drive/static/media/4.82423b3f.jpg"
+  },
+  {
+    "revision": "8e41c06a136a26974f9fd30e7694d5fa",
+    "url": "/Need-for-drive/static/media/Facebook.8e41c06a.svg"
+  },
+  {
+    "revision": "bfb46bc62ee87606aa9bbf0f05990ff0",
+    "url": "/Need-for-drive/static/media/Instagram.bfb46bc6.svg"
+  },
+  {
+    "revision": "a2af95740e7ba052b2a9caa3c6f042da",
+    "url": "/Need-for-drive/static/media/Telegram.a2af9574.svg"
+  },
+  {
+    "revision": "1267430ed27cb97f49fc2fa614fdb933",
+    "url": "/Need-for-drive/static/media/left.1267430e.svg"
+  },
+  {
+    "revision": "f9a6dc71f6e7548b73de3aded5828732",
+    "url": "/Need-for-drive/static/media/right.f9a6dc71.svg"
+  }
+]);
