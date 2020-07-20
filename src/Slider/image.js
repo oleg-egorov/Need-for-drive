2a875eb414d5b1d0c01516db00a054cb73bc4ecf
Oleg944 <legek99@yandex.ru>
2020-07-20 10:05:03 +0300

    Реализация слайдера

---
+++ src/Slider/image.js
@@ -0,0 +1,6 @@
+import Image_1 from './image/1.jpg';
+import Image_2 from './image/2.jpg';
+import Image_3 from './image/3.jpg';
+import Image_4 from './image/4.jpg';
+
+export {Image_1, Image_2, Image_3, Image_4};
